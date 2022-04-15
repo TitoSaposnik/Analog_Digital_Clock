5a758ae14c2c8f002174a044bc027daac3692d84
Roberto Martin Saposnik <tito.saposnik@gmail.com>
2022-04-14 21:52:53 -0300

    Correcciones

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ setInterval(()=>{
 })
 
 //Digital Clock
-
 let hours = document.getElementById('hours');
 let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
@@ -24,7 +23,7 @@ setInterval(()=>{
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let am = h >= 12 ? 'pm' : 'am';
+    let am = h >= 12 ? 'PM' : 'AM';
 
     if(h>12){
         h = h - 12;
